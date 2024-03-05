ebbc435d7d6f10071d5e4ff6320a096061aa61fb
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-05 09:06:12 -0300

    feat: Teste para instacia da classe, e se o customer está recebendo a propriedade

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,6 +1,14 @@
 import { test } from "@vitest/runner";
 import { Appointment } from "./appointment";
+import { expect } from "chai";
 
 test("create on appointment", () => {
- 
+    const appointment = new Appointment({
+        customer: "Jon",
+        startsAt: new Date(),
+        endsAt: new Date()
+    })
+
+    expect(appointment).instanceOf(Appointment)
+    expect(appointment.customer).equal("Jon")
 });
