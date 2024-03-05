8a137cbbda18672843c6c285966d3764b088b3a5
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-05 09:52:32 -0300

    feat: criando segundo teste e corriginso erro do primeiro

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -3,12 +3,32 @@ import { Appointment } from "./appointment";
 import { expect } from "chai";
 
 test("create on appointment", () => {
-    const appointment = new Appointment({
-        customer: "Jon",
-        startsAt: new Date(),
-        endsAt: new Date()
-    })
+  const startsAt = new Date();
+  const endsAt = new Date();
 
-    expect(appointment).instanceOf(Appointment)
-    expect(appointment.customer).equal("Jon")
+  endsAt.setDate(endsAt.getDate() + 1);
+
+  const appointment = new Appointment({
+    customer: "Jon",
+    startsAt,
+    endsAt
+  });
+
+  expect(appointment).instanceOf(Appointment);
+  expect(appointment.customer).equal("Jon");
+});
+
+test("cannot create on appointment with end date before start date", () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() - 1);
+
+  expect(() => {
+    return new Appointment({
+      customer: "Jon",
+      startsAt,
+      endsAt,
+    });
+  }).throw();
 });
