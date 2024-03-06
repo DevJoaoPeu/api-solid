4a343fa0c45635383e6b1ff72bcecf79a56a1aff
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 00:11:55 -0300

    feat: usando getFutureDate

--- src/services/create-appointment.spec.ts
+++ src/services/create-appointment.spec.ts
@@ -2,16 +2,15 @@ import { describe, it } from "@vitest/runner";
 import { Appointment } from "../entities/appointment";
 import { CreateAppointment } from "./create-appointment";
 import { expect } from "chai";
+import { getFutureDate } from "../tests/utils/get-future-date";
+
 
 describe("Create Appointment", () => {
   it("should be able to create an appointment", async () => {
     const createAppointment = new CreateAppointment();
 
-    const startsAt = new Date();
-    const endsAt = new Date();
-
-    startsAt.setDate(startsAt.getDate() + 1)
-    endsAt.setDate(endsAt.getDate() + 2);
+    const startsAt = getFutureDate("2024-04-10");
+    const endsAt = getFutureDate("2024-04-11");
 
     const result = await createAppointment.execute({
       customer: "Jon",
