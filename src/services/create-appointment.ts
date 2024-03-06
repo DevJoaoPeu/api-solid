9e965c3f4a39bd879c648733724e00f5c265569d
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 00:44:19 -0300

    feat: usando construtor e passando o appointment para o Apporepo

--- src/services/create-appointment.ts
+++ src/services/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from "../entities/appointment";
+import { AppointmentsRepository } from "../repositories/appointments-repository";
 
 interface CreateAppointmentRequest {
   customer: string;
@@ -9,16 +10,29 @@ interface CreateAppointmentRequest {
 type CreateAppointmentResponse = Appointment;
 
 export class CreateAppointment {
+  constructor(private appointmentRepo: AppointmentsRepository) {}
+
   async execute({
     customer,
     startsAt,
     endsAt,
   }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlapping = await this.appointmentRepo.findOverlapping(
+      startsAt,
+      endsAt
+    );
+
+    if(overlapping){
+      throw new Error("Appointment overlapping")
+    }
+
     const appointment = new Appointment({
       customer,
       startsAt,
       endsAt,
     });
+
+    await this.appointmentRepo.create(appointment);
 
     return appointment;
   }
