0f7f743197af1a933e66dd83f41ff7af9827bb59
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 00:41:36 -0300

    feat: criando metodo find overlapping e criando a propriedade items

--- src/repositories/in-memory/in-memory-appointments-repository.ts
+++ src/repositories/in-memory/in-memory-appointments-repository.ts
@@ -1,11 +1,29 @@
+import { areIntervalsOverlapping } from "date-fns";
 import { Appointment } from "../../entities/appointment";
 import { AppointmentsRepository } from "../appointments-repository";
 
 export class InMemoryAppointmenstRepository implements AppointmentsRepository {
-
-  public items: Appointment[] = []
+  public items: Appointment[] = [];
 
   async create(appointment: Appointment): Promise<void> {
-      this.items.push(appointment)
+    this.items.push(appointment);
+  }
+
+  async findOverlapping(
+    startsAt: Date,
+    endsAt: Date
+  ): Promise<Appointment | null> {
+    const overlapping = this.items.find((appointment) => {
+      return areIntervalsOverlapping(
+        { start: startsAt, end: endsAt },
+        { start: appointment.startsAt, end: appointment.endsAt },
+        { inclusive: true }
+      );
+    });
+
+    if (!overlapping) {
+      return null;
+    }
+    return overlapping;
   }
 }
