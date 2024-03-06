0f958fbaa9b1a0affb5c300e266ebb671642f40b
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 00:12:47 -0300

    feat: criando repositorio de agendamentos

---
+++ src/repositories/appointments-repository.ts
@@ -0,0 +1,6 @@
+import { Appointment } from "../entities/appointment";
+
+export interface AppointmentsRepository {
+  create(appointment: Appointment): Promise<void>;
+  save(appointment: Appointment): Promise<void>;
+}
