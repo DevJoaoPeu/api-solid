9329ebfc462db15f4a4db7d59f0e6176d0936fac
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 01:11:27 -0300

    feat: nova case de teste

--- src/services/create-appointment.spec.ts
+++ src/services/create-appointment.spec.ts
@@ -5,10 +5,9 @@ import { expect } from "chai";
 import { getFutureDate } from "../tests/utils/get-future-date";
 import { InMemoryAppointmenstRepository } from "../repositories/in-memory/in-memory-appointments-repository";
 
-
 describe("Create Appointment", () => {
   it("should be able to create an appointment", async () => {
-    const appointmentRepo = new InMemoryAppointmenstRepository()
+    const appointmentRepo = new InMemoryAppointmenstRepository();
     const createAppointment = new CreateAppointment(appointmentRepo);
 
     const startsAt = getFutureDate("2024-04-10");
@@ -21,5 +20,31 @@ describe("Create Appointment", () => {
     });
 
     expect(result).to.be.an.instanceOf(Appointment);
+  });
+
+  it("should not be able to create an appointment with overlapping dates", async () => {
+    const startsAt = getFutureDate("2024-04-10");
+    const endsAt = getFutureDate("2024-04-15");
+
+    const appointmentRepo = new InMemoryAppointmenstRepository();
+    const createAppointment = new CreateAppointment(appointmentRepo);
+
+    await createAppointment.execute({
+      customer: "Jon",
+      startsAt,
+      endsAt,
+    });
+
+    const overlappingAppointment = {
+      customer: "Jon",
+      startsAt: getFutureDate("2024-04-14"),
+      endsAt: getFutureDate("2024-04-14"),
+    };
+
+    try {
+      await createAppointment.execute(overlappingAppointment);
+    } catch (error) {
+      expect(error.message).to.equal("Appointment overlapping");
+    }
   });
 });
