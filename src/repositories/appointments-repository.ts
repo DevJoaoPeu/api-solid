1d2ba943a350070dcdc5742b1588a74c36944c84
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-06 00:39:53 -0300

    feat: adicionando novo metodo

--- src/repositories/appointments-repository.ts
+++ src/repositories/appointments-repository.ts
@@ -2,4 +2,5 @@ import { Appointment } from "../entities/appointment";
 
 export interface AppointmentsRepository {
   create(appointment: Appointment): Promise<void>;
+  findOverlapping(startsAt: Date, endsAt: Date): Promise<Appointment | null>
 }
